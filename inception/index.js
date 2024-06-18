8891ecdf315b57a831a9a90434d5776ced468938
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2024-06-19 02:05:52 +0530

    updated by directly integrating react

--- inception/index.js
+++ inception/index.js
@@ -1,3 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
 const heading = React.createElement("div", {id:'parent'},[React.createElement('div',{id:'child1'},React.createElement('h1',{},'hello'))]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
