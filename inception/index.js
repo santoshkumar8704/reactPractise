fc506181e5511c1c172ccb08d8ccb199bbb75df8
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2024-06-19 00:35:02 +0530

    episode-01

---
+++ inception/index.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("div", {id:'parent'},[React.createElement('div',{id:'child1'},React.createElement('h1',{},'hello'))]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
