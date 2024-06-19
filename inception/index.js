dbfe40088006dfe1bf5ee860d00eddcecd6c4efd
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2024-06-19 13:41:10 +0530

    newcommit

--- inception/index.js
+++ inception/index.js
@@ -1,7 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
+// const heading = React.createElement("div", {id:'parent'},[React.createElement('div',{id:'child1'},React.createElement('h1',{},'hello'))]);
+const Heading1 = () => (<h1>heading bro</h1>)
+const Heading = () => {
+  return (
+    <div>
+      <Heading1></Heading1>
+      <Heading1 />
+      {Heading1()}
+      <h1>component composition</h1>
+      <Heading2 />
+      <h1>head hello bud</h1>
+    </div>
+  );
+};
+const Heading2 = () => <h1>This is no return</h1>;
 
-const heading = React.createElement("div", {id:'parent'},[React.createElement('div',{id:'child1'},React.createElement('h1',{},'hello'))]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading />);
